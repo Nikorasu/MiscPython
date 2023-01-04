6b537603b6580d36bf85b7e87a3becfe5e32f5b6
Nikorasu <nikorasu85@gmail.com>
2023-01-04 15:58:02 -0600

    New version with alternative fade-effect on chains

---
+++ Matrix/matrix_fade.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# A basic Matrix code-rain terminal animation, with fade effect.
+# by Nik Stromberg - nikorasu85@gmail.com - MIT 2023
+
+import random, os, string, time
+
+class MatrixColumn:
+    def __init__(self, column):
+        self.column = column
+        self.start = -random.randint(0,os.get_terminal_size().lines) # random start position
+        self.end = random.randint(5,os.get_terminal_size().lines) # random end length
+        self.speed = random.choice([1,1,2]) # 1/3 chance of double speed
+        self.characters = random.choices(string.printable.strip(),k=self.end)
+        self.done = False
+    def update(self):
+        termH = os.get_terminal_size().lines # get terminal height
+        if 0 < self.start <= termH+len(self.characters): # if start is on screen
+            newchar = random.choice(string.printable.strip())
+            print(f'\x1b[0m\x1b[97m\x1b[{self.start};{self.column}H{newchar}',end='\b',flush=True)
+            for i in range(len(self.characters)):
+                if self.start-i-1 > 0:
+                    brightness = 255-int(255*((i+1)/self.end)**2) if i+1 < self.end else 0
+                    print(f'\x1b[0m\x1B[38;2;0;{brightness};0m\x1b[{self.start-i-1};{self.column}H{self.characters[i]}',end='\b',flush=True)
+            self.characters.insert(0,random.choice(['','\x1b[1m','\x1b[2m'])+newchar)
+            if self.speed == 2: # if double speed
+                addchar = random.choice(string.printable.strip())
+                self.characters.insert(1,addchar)
+            self.characters = self.characters[:self.end]
+            self.characters.extend([' ',' ']) # add 2 blank spaces to end of list to clear old characters off screen
+        self.start += self.speed 
+        if self.start-len(self.characters) > termH: self.done = True # if end is off screen
+
+chains = []
+taken = set()
+print('\x1b[2J\x1b[?25l') # clear screen and hide cursor
+
+while 1: # main loop
+    termW = os.get_terminal_size().columns
+    for i in range(int(termW*.8)-len(chains)): # fill 80% of the terminal width with MatrixColumns
+        while (column := random.randint(1,termW)) in taken: pass # ensures no overlappping columns
+        chains.append(MatrixColumn(column)) # spawn MatrixColumn at open column, add to list for updating
+        taken.add(column) # add column to taken set
+    for mcol in chains:
+        mcol.update()
+        if mcol.done: # remove MatrixColumns when they finish falling
+            taken.remove(mcol.column)
+            chains.remove(mcol)
+    time.sleep(.08)
