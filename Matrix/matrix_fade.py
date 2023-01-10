4699530a3458c48858029953fcefafd007c07791
Nikorasu <nikorasu85@gmail.com>
2023-01-09 20:28:31 -0600

    Changed a for loop range(len(list)) to enumerate.

--- Matrix/matrix_fade.py
+++ Matrix/matrix_fade.py
@@ -21,10 +21,10 @@ class MatrixColumn:
         if 0 < self.start <= termH+len(self.characters): # if start is on screen
             newchar = random.choice(string.printable.strip())
             print(f'\x1b[0m\x1b[97m\x1b[{self.start};{self.column}H{newchar}',end='\b',flush=True)
-            for i in range(len(self.characters)): # loop through all characters
+            for i, char in enumerate(self.characters): # loop through all characters
                 if self.start-i-1 > 0: # if characters are on screen
                     brightness = 255-int(255*((i+1)/self.end)**2) if i+1 < self.end else 0
-                    print(f'\x1b[0m\x1b[38;2;0;{brightness};0m\x1b[{self.start-i-1};{self.column}H{self.characters[i]}',end='\b',flush=True)
+                    print(f'\x1b[0m\x1b[38;2;0;{brightness};0m\x1b[{self.start-i-1};{self.column}H{char}',end='\b',flush=True)
             self.characters.insert(0,random.choice(['','','\x1b[1m','\x1b[2m'])+newchar)
             if self.speed == 2: # if double speed
                 addchar = random.choice(string.printable.strip()) # add an additional character
