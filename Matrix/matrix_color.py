e283b925ad9a6b33dece460b994cda468eedde9b
Nikorasu <nikorasu85@gmail.com>
2023-01-15 18:10:02 -0600

    Moved random bolding to before white character.

--- Matrix/matrix_color.py
+++ Matrix/matrix_color.py
@@ -21,14 +21,14 @@ class MatrixColumn:
     def update(self):
         termH = os.get_terminal_size().lines # get terminal height
         if 0 < self.start <= termH+len(self.chain): # if start is on screen
-            newchar = random.choice(self.characters) # new random character to add
+            newchar = random.choice(['','','\x1b[1m','\x1b[2m'])+random.choice(self.characters) # new character with random bold
             print(f'\x1b[97m\x1b[{self.start};{self.column}H{newchar}',end='\x1b[0m\b',flush=True) #x1b[38;2;255;255;255m
             for i, char in enumerate(self.chain): # loop through all characters
                 if self.start-i-1 > 0: # if characters are on screen
                     brightness = 1-(i/self.end)**2 if i < self.end else 0 # calculate brightness based on position in chain
                     r, g, b = hsv2rgb(self.color,1,brightness) # convert HSV to RGB for color fade
                     print(f'\x1b[38;2;{int(r)};{int(g)};{int(b)}m\x1b[{self.start-i-1};{self.column}H{char}',end='\x1b[0m\b',flush=True)
-            self.chain.insert(0,random.choice(['','','\x1b[1m','\x1b[2m'])+newchar) # insert newchar, maybe with bold
+            self.chain.insert(0,newchar) # insert newchar at start of chain
             if self.speed == 2: # if this chain is double speed
                 addchar = random.choice(self.characters) # pick an additional character for double speed
                 self.chain.insert(1,random.choice(['','','\x1b[1m','\x1b[2m'])+addchar) # add it with random formatting
@@ -38,12 +38,12 @@ class MatrixColumn:
         if self.start-len(self.chain) > termH: self.done = True # if end is off screen, mark as done, for removal
 
 def hsv2rgb(h, s, v): # convert HSV color values to RGB
-    if s == 0.0: v*=255; return (v, v, v) # if s is 0, return greyscale
+    if s == 0.0: v *= 255; return (v, v, v) # if s is 0, return greyscale
     h /= 360. # normalize h to 0-1
     i = int(h*6.) # calculate hue sector 0-5
     f = (h*6.)-i # calculate fractional part of h
     p, q, t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f))))
-    v*=255; i%=6 # calculate RGB values based on sector
+    v *= 255; i %= 6 # calculate RGB values based on sector
     if i == 0: return (v, t, p)
     if i == 1: return (q, v, p)
     if i == 2: return (p, v, t)
@@ -58,7 +58,7 @@ print('\x1b[2J\x1b[?25l') # clear screen and hide cursor
 try:
     while True: # main loop
         FullCols = set(range(1,termW := os.get_terminal_size().columns+1)) # set of all columns, & store terminal width
-        if unused.union(taken) != FullCols: unused = FullCols - taken # accounts for terminal resizing
+        if unused.union(taken) != FullCols: unused = FullCols-taken # accounts for terminal resizing
         for i in range(int(termW*DENSITY)-len(chains)): # fill Density% of the terminal width with MatrixColumns
             column = random.choice(list(unused)) # pick a random unused column
             chains.append(MatrixColumn(column)) # create a new MatrixColumn in that column
--- Matrix/matrix_fade.py
+++ Matrix/matrix_fade.py
@@ -20,13 +20,13 @@ class MatrixColumn:
     def update(self):
         termH = os.get_terminal_size().lines # get terminal height
         if 0 < self.start <= termH+len(self.chain): # if start is on screen
-            newchar = random.choice(self.characters) # new random character to add
+            newchar = random.choice(['','','\x1b[1m','\x1b[2m'])+random.choice(self.characters) # new character with random bold
             print(f'\x1b[97m\x1b[{self.start};{self.column}H{newchar}',end='\x1b[0m\b',flush=True)
             for i, char in enumerate(self.chain): # loop through all characters
                 if self.start-i-1 > 0: # if characters are on screen
                     brightness = 255-int(255*(i/self.end)**2) if i < self.end else 0 # calculate brightness fade
                     print(f'\x1b[38;2;0;{brightness};0m\x1b[{self.start-i-1};{self.column}H{char}',end='\x1b[0m\b',flush=True)
-            self.chain.insert(0,random.choice(['','','\x1b[1m','\x1b[2m'])+newchar) # insert newchar, maybe with bold
+            self.chain.insert(0,newchar) # insert newchar at start of chain
             if self.speed == 2: # if this chain is double speed
                 addchar = random.choice(self.characters) # pick an additional character for double speed
                 self.chain.insert(1,random.choice(['','','\x1b[1m','\x1b[2m'])+addchar) # add it with random formatting
@@ -42,7 +42,7 @@ print('\x1b[2J\x1b[?25l') # clear screen and hide cursor
 try:
     while True: # main loop
         FullCols = set(range(1,termW := os.get_terminal_size().columns+1)) # set of all columns, & store terminal width
-        if unused.union(taken) != FullCols: unused = FullCols - taken # accounts for terminal resizing
+        if unused.union(taken) != FullCols: unused = FullCols-taken # accounts for terminal resizing
         for i in range(int(termW*DENSITY)-len(chains)): # fill Density% of the terminal width with MatrixColumns
             column = random.choice(list(unused)) # pick a random unused column
             chains.append(MatrixColumn(column)) # create a new MatrixColumn in that column
